a3a082ac905ced5275b582787e0596b04028c75b
sulenursekmen <sulenursekmenn@gmail.com>
2023-10-05 17:37:16 +0300

    first commit

---
+++ pinia-playlist/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+ <header>
+  <div class="container">
+<img src="https://miro.medium.com/v2/resize:fit:1358/1*Rv_MrZ4I8tlXBIOO2s-mvQ.gif" alt="" title="Pinia">
+<h1>Kişisel Günlüğüm</h1>
+  </div>
+ </header>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
---
+++ pinia-playlist/src/stores/DiaryStore.js
@@ -0,0 +1,11 @@
+import {defineStore} from 'pinia'
+//export diger dosyaların kullanmasını saglıyor
+export const useDiaryStore=defineStore("diaryStore",{
+    state :()=>({
+        diary:[
+            {id:1, diary:"bugün hava bulutlu", date:"05.10.2023", isFav:false},
+            {id:2, diary:"bugün hava güzel", date:"04.10.2023", isFav:true}
+        ],
+        name:"Beşiktaş"
+    })
+});
